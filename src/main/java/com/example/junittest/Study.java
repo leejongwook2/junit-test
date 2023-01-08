2a9b41462bda7ae1c2bfe8f5d9be31f8bd1c399f
leejongwook2 <34103253+leejongwook2@users.noreply.github.com>
2023-01-08 14:17:17 +0900

    Junit5 소개, 시작하기

---
+++ src/main/java/com/example/junittest/Study.java
@@ -0,0 +1,14 @@
+package com.example.junittest;
+
+/**
+ * 스프링부트 2.2 부터 Junit5를 기본으로 사용하도록 되어있음.
+ * Jupiter Vintage
+ * Junit Platform
+ * 
+ * Vintage: Junit4, Junit3 를 지원하는 TestEngine 엔진 구현체 
+ * Jupiter: TestEngine API 구현체
+ * Junit Platform: 테스트를 실행해주는 런처 제공. TestEngine API 제공
+ */
+public class Study {
+    
+}
---
+++ src/test/java/com/example/junittest/StudyTest.java
@@ -0,0 +1,46 @@
+package com.example.junittest;
+
+import org.junit.jupiter.api.*;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+/**
+ * Web 프로젝트를 생성하지 않아도 
+ * Junit-jupiter-engine 라이브러리만 설치해주면 해당 테스트를 진행할 수 있음.
+ */
+class StudyTest {
+    @Test
+    void create() {
+        Study study = new Study();
+        assertNotNull(study);
+    }
+    // 바깥에서 테스트를 실행하면 전체 테스트 실행하고
+    // 테스트 메소드 안에서 실행 시 그 메소드만 테스트를 실행함
+    @Test
+    @Disabled
+    void create1() {
+        // @Disabled 어노테이션 붙으면 해당 테스트는 실행하지 않음.
+        System.out.println("create1");
+    }
+    
+    @BeforeAll
+    static void beforeAll() {
+        System.out.println("beforeAll");
+    }
+
+
+    @AfterAll
+    static void afterAll() {
+        System.out.println("afterAll");
+    }
+    
+    @BeforeEach
+    void beforeEach() {
+        System.out.println("beforeEach");
+    }
+
+    @AfterEach
+    void afterEach() {
+        System.out.println("afterEach");
+    }
+}
