3eedff147741bec38319bc574128c1f3e34def82
leejongwook2 <34103253+leejongwook2@users.noreply.github.com>
2023-01-11 21:29:06 +0900

    테스트 순서

---
+++ src/test/java/com/example/junittest/chap1/StudyTest_4.java
@@ -0,0 +1,48 @@
+package com.example.junittest.chap1;
+
+import com.example.junittest.Study;
+import com.example.junittest.util.FastTest;
+import com.example.junittest.util.SlowTest;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.*;
+
+
+@DisplayNameGeneration(DisplayNameGenerator.ReplaceUnderscores.class)
+@TestInstance(TestInstance.Lifecycle.PER_CLASS)
+@TestMethodOrder(MethodOrderer.OrderAnnotation.class)
+public class StudyTest_4 {
+
+    private int value = 1;
+
+    @Order(2)
+    @SlowTest
+    @DisplayName("order2")
+    void order2() {
+        System.out.println(this);
+        System.out.println(value++);
+    }
+
+    @Order(1)
+    @SlowTest
+    @DisplayName("order1")
+    void order1() {
+        System.out.println(this);
+        Study study = new Study(value);
+        System.out.println(value++);
+    }
+
+    @Order(3)
+    @SlowTest
+    @DisplayName("order3")
+    void order3() {
+        System.out.println(this);
+        System.out.println(value++);
+    }
+
+
+    @BeforeAll
+    static void beforeAll() {}
+
+    @AfterAll
+    static void afterAll() {}
+}
