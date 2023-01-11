ffcb9ce3282785ee8d2d42cba75261c6e39c7376
leejongwook2 <34103253+leejongwook2@users.noreply.github.com>
2023-01-11 15:38:38 +0900

    @Junit 테스트 이름 표시하기

--- src/test/java/com/example/junittest/StudyTest.java
+++ src/test/java/com/example/junittest/StudyTest.java
@@ -8,10 +8,13 @@ import static org.junit.jupiter.api.Assertions.*;
  * Web 프로젝트를 생성하지 않아도 
  * Junit-jupiter-engine 라이브러리만 설치해주면 해당 테스트를 진행할 수 있음.
  */
+//@DisplayNameGeneration(DisplayNameGenerator.ReplaceUnderscores.class) 메소드명을 대문자로 끊어서 공백으로 변환한 뒤 표시한다.
 class StudyTest {
+    
     @Test
+    @DisplayName("assertThat 사용")
     void create() {
-        Study study = new Study();
+        Study actual = new Study();
         assertNotNull(study);
     }
     // 바깥에서 테스트를 실행하면 전체 테스트 실행하고
